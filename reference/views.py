863f687c284e8b1b8b3d6d7b419c45cb4f3b5d80
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-11-20 09:28:10 -0500

    First Commit

---
+++ reference/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import csv
+from django.template.defaulttags import register
+
+# Django custom Filter
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+vi_data_file = current_directory + '/static/reference/data_vi_mode.csv'
+emacs_data_file = current_directory + '/static/reference/data_emacs_mode.csv'
+
+vi_categories = {}
+emacs_categories = {}
+
+# Vi Mode Data
+with open(vi_data_file, 'r') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    
+    for line in reader:
+        if line['category'] in vi_categories:
+            vi_categories[line['category']] += [[line['command'], line['description']]]
+        else:
+            vi_categories[line['category']] = [[line['command'], line['description']]]
+
+# Emacs Mode Data
+with open(emacs_data_file, 'r') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    
+    for line in reader:
+        if line['category'] in emacs_categories:
+            emacs_categories[line['category']] += [[line['command'], line['description']]]
+        else:
+            emacs_categories[line['category']] = [[line['command'], line['description']]]
+
+# Create your views here.
+def index(request):
+
+    context = {
+        'vi_categories': vi_categories,
+        'emacs_categories': emacs_categories,
+    }
+
+    return render(request, 'index.html', context=context)
+    # return render(request, 'index.html')
